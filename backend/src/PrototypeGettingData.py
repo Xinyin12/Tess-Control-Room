6702fd698fc3b93a19923312e709ec349a9252a7
ZongyuePu <zongyuep@gmail.com>
2022-11-07 20:20:30 -0800

    remove redundant, add get resources

--- backend/src/PrototypeGettingData.py
+++ backend/src/PrototypeGettingData.py
@@ -23,6 +23,13 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+@app.route('/get/resources')
+def get_all_resources():
+    cur = get_db().cursor().execute(f"""
+            SELECT name from resources;
+        """)
+    rv = cur.fetchall()
+    return str(rv)
 
 #/get/actual?name=Feeder_1
 @app.route('/get/actual')
@@ -49,70 +56,6 @@ def get_cleared_value():
             ORDER BY market_time;
             """, (datetime.datetime.now() - datetime.timedelta(hours=24)))
     return cur.fetchall()
-
-
-@app.route('/get/settingsValue')
-def get_settings_value():
-    # here we want to get the value of name (i.e. ?name=some-value)
-    name = request.args.get('name')
-    print("Name: ", name)
-    cur = get_db().cursor().execute(f"""
-        select value
-        from settings
-        where name = (?)
-        order by value_at desc;
-        """, (name,))
-
-    rv = cur.fetchall()
-    return rv
-
-
-@app.route('/get/markets_resourceid_units_interval')
-def get_markets_resourceid_units_interval():
-    cur = get_db().cursor().execute(f"""
-        select resource_id, units, interval 
-        from markets;
-        """)
-
-    rv = [dict((cur.description[agent_id][0], resource_id) for agent_id, resource_id in enumerate(row)) for row in
-          cur.fetchall()]
-    return rv
-
-
-@app.route('/get/auctions_markettime_resourceid_quantity_price_auctionid')
-def get_auctions_markettime_resourceid_quantity_price_auctionid():
-    # here we want to get the value of market_id (i.e. ?market_id=some-value)
-    market_id = int(request.args.get('market_id'))
-    print("Market Id: ", market_id)
-    cur = get_db().cursor().execute(f"""
-           select market_time, resource_id, quantity, price, auction_id 
-        from auctions 
-        where market_id = (?)
-        order by resource_id;
-           """, (market_id,))
-
-    rv = cur.fetchall()
-    return rv
-
-
-@app.route('/get/load')
-def get_auction_pv():
-    status = request.args.get('status', default = 'all', type = str)
-    device_type = request.args.get('device_type', default = 'all', type = str)
-    if (status == 'dispatched'):
-        cur = get_db().cursor().execute(f"""SELECT * FROM agents;""")
-    elif (status == 'available'):
-        return 'available'
-    elif (status == 'unavailable'):
-        return 'unavailable'
-    elif (status == 'all'):
-        return 'all'
-    else:
-        return 'WRONG STATUS!!'     
-
-    cur = get_db().cursor().execute(f"""SELECT * FROM agents;""")
-    rv = cur.fetchall()
-    return str(rv)
 
 if __name__ == "__main__":
     app.run(debug=True)
