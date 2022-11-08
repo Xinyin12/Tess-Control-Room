2c76f7d0b6b32b54d9b61556361592acab0aca9d
ZongyuePu <zongyuep@gmail.com>
2022-11-07 17:35:36 -0800

    clear up

--- simulation/gridlabd/dump.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-import database as db
-
-if len(sys.argv) == 1:
-    print("Syntax: python3 dump.py DATABASE [PREFIX]")
-    exit(0)
-
-test = db.Tess2Database(sys.argv[1])
-test.dump(sys.argv[2]+"_" if len(sys.argv) > 2 else os.path.splitext(os.path.basename(sys.argv[1]))[0]+"_")
