bab00d24badfe47f9da8dcaeda3feaa3e4dc7a78
ZongyuePu <zongyuep@gmail.com>
2022-11-11 10:58:21 -0800

    update query

--- backend/src/PrototypeGettingData.py
+++ backend/src/PrototypeGettingData.py
@@ -23,30 +23,44 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+# TODO
+# [resource_id] --> get [name] from most recent valid_at --> front end build drop down from [name]-->new query
+# [resource_id, current_name]
+# page, limit (20)
+# optional argument, last_resource_id
+# order return value yb resource id, return next 20 values greater than last_resource_id
 @app.route('/get/resources')
 def get_all_resources():
     cur = get_db().cursor().execute(f"""
-            SELECT name from resources;
+            SELECT resource_id from resources;
         """)
     rv = cur.fetchall()
     return str(rv)
 
-#/get/actual?name=Feeder_1
+#/get/actual?resource_id=<id>
+# TODO: now-12hours, Order
+# choose name to rerender
+# use resource id
+# limit 144
 @app.route('/get/actual')
 def get_actual_value():
-    resource_name = request.args.get('name')
+    resource_id = request.args.get('resource_id')
+    # now - 12 hours
     cur = get_db().cursor().execute(f"""
             SELECT meters.valid_at AS x, sum(meters.real_power) AS y
             FROM meters JOIN devices ON meters.device_id = devices.device_id
             JOIN agents ON devices.agent_id = agents.agent_id
-            JOIN resources ON agents.resource_id = resources.resource_id
-            WHERE resources.name = (?)
-            group by round(meters.valid_at/300);
-        """, (resource_name,))
+            WHERE agents.resource_id = (?)
+            GROUP BY round(meters.valid_at/300)
+            (ORDER BY) 
+            ;
+        """, (resource_id,))
 
     rv = cur.fetchall()
     return str(rv)
 
+# TODO: now-12hours, Order
+# use resource id
 @app.route('/get/cleared')
 def get_cleared_value():
     # what reported in the auction table
